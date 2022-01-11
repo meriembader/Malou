70de42aa98e170998b3530e34dccd901c2deafab
meriembader <meriembader8@gmail.com>
2022-01-11 14:32:28 +0100

    test done

--- FrontApp/src/app/component/product-category-chart/product-category-chart.component.spec.ts
+++ FrontApp/src/app/component/product-category-chart/product-category-chart.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProductCategoryChartComponent } from './product-category-chart.component';
-
+import {DatePipe} from '@angular/common';
 describe('ProductCategoryChartComponent', () => {
   let component: ProductCategoryChartComponent;
   let fixture: ComponentFixture<ProductCategoryChartComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductCategoryChartComponent ]
+      declarations: [ ProductCategoryChartComponent ],
+      imports: [HttpClientTestingModule], 
+      providers:[DatePipe]
     })
     .compileComponents();
   });
