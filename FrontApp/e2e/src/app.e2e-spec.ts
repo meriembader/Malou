36cfd8813af157bebe8fadcfca7c8f311a0dd886
meriembader <meriembader8@gmail.com>
2022-01-12 02:21:41 +0100

    test | lint | e2e tests done

--- FrontApp/e2e/src/app.e2e-spec.ts
+++ FrontApp/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('project-frontend app is running!');
   });
 
   afterEach(async () => {
